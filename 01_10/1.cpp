4229e4726db26ab5c935f9b712a4a14d4d09d4c3
YashovardhanSiramdas <ced16i028@iiitdm.ac.in>
2018-01-09 23:31:24 +0530

    Added1.c

---
+++ 01_10/1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class comparisions
+{
+	public:		
+		int insertion_sort(int i);
+		int merge_sort(int i);
+};
+
+int comparisions::insertion_sort(int i)
+{
+	return (8 * i * i);
+}
+
+int comparisions::merge_sort(int i)
+{
+	return (64 * i * log(i) / log(2));
+}
+
+int main()
+{
+	comparisions obj;
+	int i;
+	cout<<"Values of n for which Insertion sort beats Merge Sort:- \n";
+	for (i = 2; ; i++)
+	{
+		if (obj.insertion_sort(i) < obj.merge_sort(i))
+			cout<<i<<" ";
+		else
+		{
+			cout<<endl;
+			break;
+		}
+	}
+
+	return (0);
+}
