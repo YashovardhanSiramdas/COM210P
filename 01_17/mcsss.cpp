64d7b7a1207aef63fa8d3f8986b197b1cfb5230f
YashovardhanSiramdas <ced16i028@iiitdm.ac.in>
2018-01-17 11:23:57 +0530

    Updated mcsss

--- 01_17/mcsss.cpp
+++ 01_17/mcsss.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 using namespace std;
 
+#define INT_MIN -32767
+
 int bruteForce(int a[],int n)
 {
 
@@ -67,6 +69,22 @@ int divideAndConquer(int a[],int beg,int end)
 	return max(max(lsp,lsum+rsum),max(lsum+rsum,rsp));
 }
 
+int kadanesAlgorithm(int a[], int n)
+{
+    int max_so_far = INT_MIN, max_ending_here = 0;
+ 
+    for (int i = 0; i < n; i++)
+    {
+        max_ending_here = max_ending_here + a[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+ 
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
+}
+
 /*int kadanesAlgorithm(int a[], int n)
 {
    int max_so_far = a[0];
@@ -80,7 +98,7 @@ int divideAndConquer(int a[],int beg,int end)
    return max_so_far;
 }*/
 
-int kadanesAlgorithm(int a[], int n)
+/*int kadanesAlgorithm(int a[], int n)
 {
    int max_so_far = 0, max_ending_here = 0;
    for (int i = 0; i < n; i++)
@@ -93,7 +111,7 @@ int kadanesAlgorithm(int a[], int n)
            max_so_far = max_ending_here;
    }
    return max_so_far;
-}
+}*/
 
 int main()
 {
