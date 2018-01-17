e2fe003f20299d74448d2965bb8a1156f8557e61
YashovardhanSiramdas <ced16i028@iiitdm.ac.in>
2018-01-17 11:37:55 +0530

    Added Indices

--- 01_17/mcsss.cpp
+++ 01_17/mcsss.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 #define INT_MIN -32767
+int start = 0,end=0,s=0;
 
 int bruteForce(int a[],int n)
 {
@@ -84,6 +85,29 @@ int kadanesAlgorithm(int a[], int n)
     }
     return max_so_far;
 }
+
+/*int kadanesAlgorithmIndices(int a[], int n)
+{
+    int max_so_far = INT_MIN, max_ending_here = 0;
+ 
+    for (int i = 0; i < n; i++)
+    {
+        max_ending_here = max_ending_here + a[i];
+        if (max_so_far < max_ending_here)
+        {
+            max_so_far = max_ending_here;
+            start = s;
+            end = i;
+        }
+ 
+        if (max_ending_here < 0)
+        {
+            max_ending_here = 0;
+            s = i + 1;
+        }
+    }
+    return max_so_far;
+}*/
 
 /*int kadanesAlgorithm(int a[], int n)
 {
