5c1b0008bf2ac9bd67df0a1209ada748165d255b
YashovardhanSiramdas <ced16i028@iiitdm.ac.in>
2018-02-06 16:07:46 +0530

    Finished 01_31

---
+++ 01_31/3.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+using namespace std;
+
+struct point
+{
+	float x;
+	float y;
+	float res;
+	struct point *link;
+};
+struct point *head[10];
+int main()
+{
+	int n;
+	cout<<"Enter number of ordered pairs\n";
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		struct point *p=(struct point *)malloc(sizeof(struct point));
+		cin>>p->x>>p->y;
+		p->res=sqrt((p->x*p->x)+(p->y*p->y));
+		p->res=p->res*10;
+		int x=p->res;
+			if(head[x]==NULL)
+			{
+				head[x]=p;
+			}
+			else
+			{
+				struct point *t=head[x];
+				while(t!=NULL)
+				{
+					if(p->res>t->res && p->res<=t->res)
+						break;
+				}
+				p->link=t->link;
+				t->link=p;
+
+			}
+
+	}
+	for(int i=0;i<10;i++)
+	{
+		if(head[i]!=NULL)
+		{
+			struct point *t=head[i];
+			while(t!=NULL)
+			{
+				cout<<t->x<<","<<t->y<<"\n";
+				t=t->link;
+			}
+		}
+	}
+	cout<<"\n";
+
+
+
+
+}
