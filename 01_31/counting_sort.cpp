d22fd8c678285699fa2fcfbe27bf6a1a65145c2b
Yashovardhan Siramdas <ced16i028@iiitdm.ac.in>
2018-02-18 12:59:17 +0530

    Styling for counting_sort

--- 01_31/counting_sort.cpp
+++ 01_31/counting_sort.cpp
@@ -1,10 +1,19 @@
+/*
+Author:- Yashovardhan Siramdas
+CED16I028
+
+Lab Session 5, 31-01-18
+Counting Sort
+Use Counting to sort Integer Keys.(Stable Sorting)
+*/
+
 #include <iostream>
 using namespace std;
 
 class counting_sort
 {
 	private:
-		int n,a[100],b[100],f[50],cf[50];
+		int n, a[100], b[100], f[100], cf[100];
 
 	public:
 		void init_f_cf();
@@ -18,8 +27,8 @@ class counting_sort
 
 void counting_sort::init_f_cf()
 {
-	for(int i=0;i<50;i++)
-		f[i]=cf[i]=0;
+	for (int i = 0; i < 100; i++)
+		f[i] = cf[i] = 0;
 }
 
 void counting_sort::size_init()
@@ -31,58 +40,46 @@ void counting_sort::size_init()
 void counting_sort::read_input()
 {
 	cout<<"Enter "<<n<<" Integers\n";
-	for(int i=0;i<n;i++)
+	for (int i = 0; i < n; i++)
 		cin>>a[i];
 }
 
 int counting_sort::getMax()
 {
-	int x=a[0];
-	for(int i=1;i<n;i++)
-		if(a[i]>x)
-			x=a[i];
+	int x = a[0];
+	for (int i = 1; i < n; i++)
+		if (a[i] > x)
+			x = a[i];
 	return x;
 }
 
 void counting_sort::populate_f_cf()
 {
-	for(int i=0;i<n;i++)
-	{
+	for (int i = 0; i < n; i++)
 		f[a[i]]++;
-	}
 
-	for(int i=0;i<getMax()+1;i++)
+	for (int i = 0; i < getMax() + 1; i++)
 	{
-		if(i==0)
-			cf[i]=f[i];
+		if (i == 0)
+			cf[i] = f[i];
 		else
-		{
-			cf[i]=cf[i-1]+f[i];
-		}
+			cf[i] = cf[i - 1] + f[i];
 	}
-
-	/*for(int i=0;i<getMax()+1;i++)
-		cout<<f[i]<<" ";
-	cout<<"\n";
-
-	for(int i=0;i<getMax()+1;i++)
-		cout<<cf[i]<<" ";
-	cout<<"\n";*/
 
 }
 
 void counting_sort::sort()
 {
-	for(int i=n-1;i>=0;i--)
+	for (int i = n - 1; i >= 0; i--)
 	{
 		cf[a[i]]--;
-		b[cf[a[i]]]=a[i];
+		b[cf[a[i]]] = a[i];
 	}
 }
 
 void counting_sort::display()
 {
-	for(int i=0;i<n;i++)
+	for (int i = 0; i < n; i++)
 		cout<<b[i]<<" ";
 	cout<<"\n";
 }
@@ -95,5 +92,6 @@ int main()
 	S.read_input();
 	S.populate_f_cf();
 	S.sort();
+	cout<<"Sorted Order:- \n";
 	S.display();
 }
