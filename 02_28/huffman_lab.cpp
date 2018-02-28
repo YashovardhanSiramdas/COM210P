f2fc177860fb475abb1797d33945c50d481fe6bf
Yashovardhan Siramdas <ced16i028@iiitdm.ac.in>
2018-02-28 13:15:20 +0530

    Added: Huffman

--- 02_28/huffman_lab.cpp
+++ 02_28/huffman_lab.cpp
@@ -29,6 +29,10 @@ struct node *heapify(struct node *root,char a,int f)
 	}
 }
 
+int min(int a,int b)
+{
+	return a<b?a:b;
+}
 void swap(struct heap *a,struct heap *b)
 {
 	struct heap t=*a;
@@ -48,6 +52,50 @@ void heapinsert(char key,int key_f)
 		i=i/2;
 	}
 	//printf("%d is successfully inserted in Heap\n",key);
+}
+
+struct heap delMin()
+{
+	/*if(H.hsize==0)
+	{
+		printf("Heap is empty,delMax can't be performed\n");
+		return ;
+	}*/
+
+	heap x;
+	x=h[1];
+/*	x.f=h[1].f;
+	x.c=h[1].c;*/
+
+	h[1]=h[hsize];
+	hsize--;
+	int i=1;
+	while((2*i)<=hsize)
+	{
+		if((2*i)+1<=hsize && h[i].f>min(h[2*i].f,h[(2*i)+1].f))
+		{
+			if(h[(2*i)+1].f>h[2*i].f)
+			{
+				swap(&h[i],&h[(2*i)]);
+				i=(2*i);
+			}
+			else
+			{
+				swap(&h[i],&h[(2*i)+1]);
+				i=(2*i)+1;	
+			}
+
+		}
+		else if((2*i)+1>H.hsize && H.a[i]>H.a[2*i])
+		{
+			swap(&h[i],&h[(2*i)]);
+			break;
+		}
+		else
+			break;
+	}
+	return x;
+
 }
 
 int main()
